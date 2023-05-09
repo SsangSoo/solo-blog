6429a2234c119f63492e2808e75e57601399073b
SsangSoo <kss10080@naver.com>
2023-05-09 18:52:39 +0900

    start.

---
+++ src/main/java/soloproject/blog/domain/blog/controller/ViewController.java
@@ -0,0 +1,12 @@
+package soloproject.blog.domain.blog.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ViewController implements ControllerCore{
+
+    @Override
+    public String post1() {
+        return "";
+    }
+}
